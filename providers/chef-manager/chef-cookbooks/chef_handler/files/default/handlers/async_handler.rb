ca295944b40575b7dd6feed7ad9af6c5a79d17e4
David Richard <david.richard@bull.net>
2012-07-18 09:51:33 +0200

    CLOUD-47 Change the Chef Handler to send a JSON report

--- providers/chef-manager/chef-cookbooks/chef_handler/files/default/handlers/async_handler.rb
+++ providers/chef-manager/chef-cookbooks/chef_handler/files/default/handlers/async_handler.rb
@@ -2,7 +2,7 @@ require "chef/handler"
 
 module Stomp
   class Summarize < Chef::Handler
-    attr_accessor :conf, :conffile
+    attr_accessor :conf, :conffile, :previous_report_file
 
     def initialize(args)
       self.conffile = args[:configfile]
@@ -10,10 +10,12 @@ module Stomp
 
     def report
       self.conf = YAML.load_file(self.conffile)
+      self.previous_report_file = Chef::Config[:file_backup_path] + "/previous_report.json"
       begin
         require "rubygems"
         require "stomp"
         require "base64"
+        require "json"
 
         ignored_resources = [ "/etc/chef/ohai_plugins", "/var/cache/chef/handlers", "/var/cache/chef/backup/handlers" ]
         report_resources= []
@@ -39,14 +41,45 @@ module Stomp
           end
         end
 
-        cnx=Stomp::Client.new(self.conf[:stomp_user], self.conf[:stomp_password], self.conf[:stomp_server], self.conf[:stomp_port], true)
-        msg="<report><nodename>" + node.name.dup + "</nodename>" + "<ipaddress>" + node.ipaddress.to_s + "</ipaddress>" + "<success>" + run_status.success?.to_s + "</success>" + "<start_time>" + start_time.to_s + "</start_time>" + "<run_list>" + node.run_list.to_s + "</run_list>" + "</report>" 
-        cnx.publish(self.conf[:stomp_queue], msg, {:persistent => false, :suppress_content_length => true})
-        Chef::Log.debug("Report is sent")
-		 
-        cnx.close
+
+        summary = { :nodename => node.name.dup,
+                    :ipaddress => node.ipaddress,
+                    :success => run_status.success?,
+                    :start_time => start_time,
+                    :end_time => end_time,
+                    :run_list => node.run_list,
+        }
+        msg = summary.to_json
+        #send only if there is no previous report with the same run list and success status
+        if File::exist?(self.previous_report_file)
+          previous_report = File::open(self.previous_report_file, "r+")
+          previous_summary = JSON::load(previous_report)
+          previous_report.close
+          if previous_summary["success"] == true and (previous_summary["run_list"].to_json == summary[:run_list].to_json)
+            Chef::Log.debug("The report was not send : the result is the same as the previous report")
+          else
+            send_report(msg)
+          end
+        else
+          send_report(msg)
+        end
+        #write the summary in a file
+        previous_report = File::new(self.previous_report_file, "w+")
+        JSON::dump(summary, previous_report)
+        previous_report.close
       rescue Exception => e
         Chef::Log.error("Could not summarize and send back data for this run ! (#{e})")
+      end
+    end
+
+    def send_report msg
+      begin
+        require "stomp"
+        require "base64"
+        cnx=Stomp::Client.new(self.conf[:stomp_user], self.conf[:stomp_password], self.conf[:stomp_server], self.conf[:stomp_port], true)
+        cnx.publish(self.conf[:stomp_queue], msg, {:persistent => false, :suppress_content_length => true})
+        Chef::Log.debug("Report is sent")
+        cnx.close
       end
     end
 
